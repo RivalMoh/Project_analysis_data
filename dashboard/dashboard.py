6f7eceb6067a716aadfa2654aefdb756b40fdba4
Rival Moh <vallllwhy@students.unnes.ac.id>
2024-08-16 10:13:59 +0700

    try another method

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
